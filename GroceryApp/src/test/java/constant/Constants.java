5aeaad060cbfb379baa0219c4bbc91a0ec09bbe0
SHIYAZ AL SULAIMAN <SHIYAZ AL SULAIMAN@DESKTOP-977KMD5>
2023-05-10 22:39:56 +0530

    first commit

--- GroceryApp/src/test/java/constant/Constants.java
+++ GroceryApp/src/test/java/constant/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
 	public static final String loginButton_Background_color="rgba(52, 58, 64, 1)";
 	public static final String error_message="Color Not matching";
 	
+//	public static final String new_Button_expected_text="New";
+//	public static final String assert_message="Text Not matching";
+	
 	public static final String expense_category_expectedResult="Alert!";
 	public static final String expense_category_message="Not Matching";
 	
