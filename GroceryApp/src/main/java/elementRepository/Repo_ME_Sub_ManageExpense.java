af7399616a6700b218ac2394265b357743fff0e7
SHIYAZ AL SULAIMAN <SHIYAZ AL SULAIMAN@DESKTOP-977KMD5>
2023-05-17 08:31:36 +0530

    update

--- GroceryApp/src/main/java/elementRepository/Repo_ME_Sub_ManageExpense.java
+++ GroceryApp/src/main/java/elementRepository/Repo_ME_Sub_ManageExpense.java
@@ -1,6 +1,7 @@
 package elementRepository;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -26,6 +27,36 @@ public class Repo_ME_Sub_ManageExpense {
 	@FindBy(xpath = "//tr[@class='detail-row open']")
 	WebElement detail_row_open;
 
+	@FindBy(xpath = "//a[@class='btn btn-rounded btn-danger']")
+	WebElement NewButton;
+
+	@FindBy(xpath = "//h1[text()='Add Expense']")
+	WebElement verify_Addexpense_Title;
+
+	@FindBy(xpath = "//select[@id='user_id']")
+	WebElement addexpense_ddl_User;
+
+	@FindBy(xpath = "//input[@id='ex_date']")
+	WebElement addexpense_date;
+
+	@FindBy(xpath = "//select[@id='ex_cat']")
+	WebElement addexpense_ddlcategory;
+
+	@FindBy(xpath = "//select[@id='order_id']")
+	WebElement addexpense_ddl_orderId;
+
+	@FindBy(xpath = "//select[@id='purchase_id']")
+	WebElement addexpense_ddl_PurchaseId;
+
+	@FindBy(xpath = "//select[@id='ex_type']")
+	WebElement addexpense_ddl_ExpenseType;
+
+	@FindBy(xpath = "//input[@id='amount']")
+	WebElement addexpense_Amount;
+
+	@FindBy(xpath = "//textarea[@id='content']")
+	WebElement addexpense_Remarks;
+
 	public WebElement get_ViewMore_element() {
 		int rowValue = gdtable.get_Dynamic_Table_Elements(driver,
 				"//table[@class='table table-bordered table-hover table-sm']//tbody//tr//td[1]",
@@ -46,6 +77,64 @@ public class Repo_ME_Sub_ManageExpense {
 			value = false;
 		}
 		return value;
+	}
+
+	public String click_Sub_Manageexpense_New_Button() {
+		NewButton.click();
+		return gu.getElementText(verify_Addexpense_Title);
+
+	}
+
+	public String Sub_ManageExpense_Add_Expense_Ddluser() {
+		NewButton.click();
+		return gu.dropdown_selectByIndex(addexpense_ddl_User, 1);
+	}
+
+	public void Sub_ManageExpense_AddExpense_Date_Datepicker() {
+		gu.sendKeyElements(addexpense_date, "10-02-1995");
+	}
+
+	public String Sub_ManageExpense_Add_Expense_Ddlcategory() {
+		return gu.dropdown_selectByIndex(addexpense_ddlcategory, 1);
+	}
+
+	public String Sub_ManageExpense_Add_Expense_Ddl_orderId() {
+		return gu.dropdown_selectByIndex(addexpense_ddl_orderId, 1);
+	}
+
+	public String Sub_ManageExpense_Add_Expense_Ddl_PurchaseId() {
+		return gu.dropdown_selectByIndex(addexpense_ddl_PurchaseId, 1);
+	}
+
+	public String Sub_ManageExpense_Add_Expense_Ddl_ExpenseType() {
+		return gu.dropdown_selectByIndex(addexpense_ddl_ExpenseType, 1);
+	}
+
+	public void Sub_ManageExpense_Add_Expense_Amount() {
+		gu.sendKeyElements(addexpense_Amount, Integer.toString(100));
+	}
+	public void Sub_ManageExpense_Add_Remark() {
+		gu.sendKeyElements(addexpense_Remarks, "Sample Remark Added");
+	}
+
+	public void Sub_ManageExpense_AddNew_Expense() {
+		Sub_ManageExpense_Add_Expense_Ddluser();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_AddExpense_Date_Datepicker();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_Add_Expense_Ddlcategory();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_Add_Expense_Ddl_orderId();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_Add_Expense_Ddl_PurchaseId();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_Add_Expense_Ddl_ExpenseType();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_Add_Expense_Amount();
+		gu.keyboard_Event_Tab(driver);
+		Sub_ManageExpense_Add_Remark();
+		gu.keyboard_Event_Tab(driver);
+		
 	}
 
 }
